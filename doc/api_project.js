a659d3f5997971c09fab6976bc9d24590cb71d09
CesarArtezan <arteza.cabrera@gmail.com>
2018-08-05 18:02:11 -0500

    doc v3

--- doc/api_project.js
+++ doc/api_project.js
@@ -2,13 +2,13 @@ define({
   "name": "API-Doc-Consultores-SAP",
   "version": "0.1.0",
   "description": "Documentation for Consultores SAP",
-  "url": "http://30.220.52.51/api/v1",
-  "sampleUrl": "http://30.220.52.51/api/v1",
+  "url": "http://31.220.52.51:3000/api/v1",
+  "sampleUrl": "http://31.220.52.51:3000/api/v1",
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-08-05T22:53:40.509Z",
+    "time": "2018-08-05T23:01:59.215Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
